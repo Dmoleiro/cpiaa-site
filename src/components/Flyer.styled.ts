667ee3e7e6d6ff0ebfd8d455aeff687fb0794583
dmoleiro <danielmoleiro@gmail.com>
2023-08-31 23:19:52 +0100

    feat: update flyers and slideshow

--- src/components/Flyer.styled.ts
+++ src/components/Flyer.styled.ts
@@ -1,7 +1,9 @@
 import styled from 'styled-components';
 
 interface FlyerStyleProps {
-    visible: boolean
+    visible?: boolean
+    rotation?: string
+    placement?: string
 }
 
 export const FlyerBackground = styled.div<FlyerStyleProps>`
@@ -71,11 +73,56 @@ width: 100%;
 position: relative;
   display: flex;
   overflow: hidden;
-`;
+`
 
 export const Slide = styled.div`
   flex: 0 0 100%;
   transition: transform 0.5s ease-in-out;
   display: flex;
   justify-content: center;
-`;
+`
+
+export const Arrow = styled.div<FlyerStyleProps>`
+    position: absolute;
+    top: 50%;
+    left: ${props => props.placement === 'left' ? '-10px' : ''};
+    right: ${props => props.placement === 'right' ? '-10px' : ''};
+    transform: translate(-50%, -50%);
+    transform: rotate(90deg);
+    cursor: pointer;
+    transform: rotate(${props => props.rotation}deg);
+
+
+& span {
+    display: block;
+    width: 1.5vw;
+    height: 1.5vw;
+    border-bottom: 5px solid ${props => props.theme.flyerButtonColorSecondary};
+    border-right: 5px solid ${props => props.theme.flyerButtonColorSecondary};
+    transform: rotate(45deg);
+    margin: -10px;
+    animation: animate 2s infinite;
+}
+
+& span:nth-child(2) {
+    animation-delay: -0.2s;
+}
+
+& span:nth-child(3) {
+    animation-delay: -0.4s;
+}
+
+@keyframes animate {
+    0% {
+        opacity: 0;
+        transform: rotate(45deg) translate(-20px, -20px);
+    }
+    50% {
+        opacity: 1;
+    }
+    100% {
+        opacity: 0;
+        transform: rotate(45deg) translate(20px, 20px);
+    }
+}
+`
--- src/components/Flyer.tsx
+++ src/components/Flyer.tsx
@@ -6,10 +6,28 @@ import * as Styled from './Flyer.styled';
 export const FLYER_CONTAINER_TEST_ID = 'flyer-container-test-id'
 
 const Flyer: React.FC = () => {
+    let timer: string | number | NodeJS.Timer | undefined
     const [showFlyer, toggleFlyerVisibility] = useState<boolean>(true)
     const [currentSlide, setCurrentSlide] = useState(0);
 
     const handleCloseFlyer = () => toggleFlyerVisibility(false)
+
+    const getArrow = (rotation: string, placement: string, onClick: () => void): ReactElement => {
+        return <Styled.Arrow placement={placement} rotation={rotation} onClick={(e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+            e.stopPropagation()
+            onClick()
+        }
+        }>
+            <span></span>
+            <span></span>
+            <span></span>
+        </Styled.Arrow>
+    }
+
+    const handleArrowClick = (index: number): void => {
+        setCurrentSlide(index)
+        clearInterval(timer)
+    }
 
     const getFlyer = (index: number): ReactElement => {
         if (index === 0) {
@@ -17,25 +35,28 @@ const Flyer: React.FC = () => {
                 <img src={flyer1} loading="eager" onClick={(e: React.MouseEvent<HTMLImageElement, MouseEvent>) => e.stopPropagation()} />
                 <a href="https://docs.google.com/forms/d/1-9-8OpAF_15FbjReN8LrFN5a79djbQvYFKno3LIk_tQ/edit?usp=forms_home" target="_blank"><Styled.ButtonForm>Questionário</Styled.ButtonForm></a>
                 <Styled.ButtonContinue onClick={handleCloseFlyer}>Continuar para o site</Styled.ButtonContinue>
+                {getArrow('-90', 'right', () => handleArrowClick(1))}
             </Styled.FlyerContent>
         }
 
         return <Styled.FlyerContent>
             <img src={flyer2} loading="eager" onClick={(e: React.MouseEvent<HTMLImageElement, MouseEvent>) => e.stopPropagation()} />
+            <Styled.ButtonContinue onClick={handleCloseFlyer}>Continuar para o site</Styled.ButtonContinue>
+            {getArrow('90', 'left', () => handleArrowClick(0))}
         </Styled.FlyerContent>
     }
 
     const slides = [getFlyer(0), getFlyer(1)]
 
     useEffect(() => {
-        const timer = setInterval(() => {
+        timer = setInterval(() => {
             setCurrentSlide((prevSlide) => (prevSlide + 1) % slides.length);
         }, 5000);
 
         return () => {
-            clearInterval(timer);
+            clearInterval(timer)
         };
-    }, [slides.length]);
+    }, [slides.length])
 
 
     return (
